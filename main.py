7e8d8ad451f6176f80be5ec445a087506205aaf0
Harry Herald R <harryherald019@gmail.com>
2025-03-18 13:36:41 +0530

    url paste box

--- main.py
+++ main.py
@@ -1,72 +1,50 @@
+from flask import Flask, jsonify, request
 import time
 from zapv2 import ZAPv2
 import pandas as pd
-from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-# Enable CORS for frontend testing
+
 from flask_cors import CORS
 CORS(app)
 
-
-
 apiKey = 'bk25pnr21gmnpbfqkk8sjedci9'
-target = 'https://public-firing-range.appspot.com'
 zap = ZAPv2(apikey=apiKey, proxies={'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'})
 
-
-
-#for doing a SPIDER
-def init_spider(target, zap = zap):
+# Spider Function
+def init_spider(target):
     scanID = zap.spider.scan(target)
     while int(zap.spider.status(scanID)) < 100:
-        print('Spider progress %: {}'.format(zap.spider.status(scanID)))
         time.sleep(1)
-
     print('Spider has completed!')
 
+# Passive Scan Function
+def passive_scan(target):
+    init_spider(target)
 
-
-#for doing a passive scan
-def passive_scan(zap = zap):
-    
-    #check if spider exists
-    if zap.spider.status("0") == "does_not_exist":
-        print('initializing spider')
-        init_spider(target=target)
-    else:
-        print("spider exists")
-
-    #passive scan code
     while int(zap.pscan.records_to_scan) > 0:
-        # Loop until the passive scan has finished
-        print('Records to passive scan : ' + zap.pscan.records_to_scan)
         time.sleep(2)
 
-    print('Passive Scan completed')
-
-    # Print Passive scan results/alerts
-    # print('Hosts: {}'.format(', '.join(zap.core.hosts)))
-    # print('Alerts: ')
-    # pprint(zap.core.alerts())
-
-    #storing the data in a dataframe
     alerts = zap.core.alerts()
     df = pd.json_normalize(alerts, sep='_')
-    #some basic data cleaning
-    df = df.drop(['sourceid', 'sourceMessageId', 'messageId', 'pluginId', 'alertRef','method', 'param', 'inputVector', 'attack', 'evidence','name', 'id', 'wascid', 'cweid', 'other' ],
-                axis=1)
 
-    #storing the dataframe in a json file
+    df = df.drop(
+        ['sourceid', 'sourceMessageId', 'messageId', 'pluginId', 'alertRef',
+         'method', 'param', 'inputVector', 'attack', 'evidence', 'name',
+         'id', 'wascid', 'cweid', 'other'], axis=1)
+
     df.to_json('table.json')
     return df.to_dict(orient="records")
 
-
-# API to Start Scan
+# API to Start Scan (with User Input)
 @app.route('/scan', methods=['GET'])
 def scan():
-    passive_scan()
-    return jsonify({"message": "Scan completed! Use /results to see findings."})
+    target_url = request.args.get('url')
+    if not target_url:
+        return jsonify({"error": "Missing target URL"}), 400
+
+    passive_scan(target_url)
+    return jsonify({"message": f"Scan completed for {target_url}. Use /results to see findings."})
 
 # API to Fetch Results
 @app.route('/results', methods=['GET'])
@@ -77,7 +55,5 @@ def get_results():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-# Run Flask App
 if __name__ == '__main__':
     app.run(debug=True)
-
